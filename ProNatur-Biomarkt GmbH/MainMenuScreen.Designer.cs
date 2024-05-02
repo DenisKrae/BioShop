0eb2f1ccc2f6839c3a9a257c637b40f78c0c1f82
DenisKrae <Dennis21_K@live.de>
2024-05-03 00:09:52 +0200

    main menu ist fertig

--- ProNatur-Biomarkt GmbH/MainMenuScreen.Designer.cs
+++ ProNatur-Biomarkt GmbH/MainMenuScreen.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuScreen));
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnProducts.Font = new System.Drawing.Font("Wide Latin", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProducts.ForeColor = System.Drawing.Color.Black;
+            this.btnProducts.Image = ((System.Drawing.Image)(resources.GetObject("btnProducts.Image")));
+            this.btnProducts.Location = new System.Drawing.Point(9, 12);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(469, 287);
+            this.btnProducts.TabIndex = 0;
+            this.btnProducts.Text = "Produkte verwalten";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("Wide Latin", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(484, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(490, 287);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Rechnung erstellen";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainMenuScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(986, 311);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnProducts);
             this.Name = "MainMenuScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hauptmenü";
@@ -43,5 +75,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button button1;
     }
 }
